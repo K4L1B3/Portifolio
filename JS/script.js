39d544e2c528a3960ee21ba136e64d1adf323bc3
K4L1B3 <kalibe100@gmail.com>
2022-10-18 12:35:50 -0300

    Add: foi adicionado a sessão de projetos e contact-me terminando assim o portifolio

--- JS/script.js
+++ JS/script.js
@@ -144,3 +144,19 @@ modalCloses.forEach((modalCloses, i) => {
         })
     })
 })
+
+
+// Swipper JS
+let swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop:true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable:true,
+    },
+});
+
